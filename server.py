c635268c2155b514ee5abf89c44fe14627a4ced7
Nex7x <drivenindata@gmail.com>
2025-09-09 16:13:00 +0000

    Update server.py

--- server.py
+++ server.py
@@ -467,4 +467,5 @@ if __name__ == '__main__':
     import atexit
     atexit.register(save_data_to_file)
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
+
